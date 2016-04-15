e84968cd94655ce0a5b8a3e63fef64bbc0f6ae34
Diego Luiz <diego.luiz@veinteractive.com>
2016-04-15 14:31:02 +0100

    Added the null validation when load the ignored subscriptions

--- azure-sb.js
+++ azure-sb.js
@@ -22,7 +22,7 @@ var azuresbServers = require(path.join(configDir, 'azure-sb.json')).map((c) => {
   client.prefix = c.prefix;
   client.queues = c.queues;
   client.topics = c.topics;
-  client.ignoredsubscriptions = c.ignoredsubscriptions;
+  client.ignoredsubscriptions = c.ignoredsubscriptions || [];;
 
   if(!client.queues && !client.topics){
     util.log(`[${c.endpoint}]: WARN queues = false, topics = false, not doing anything`);
